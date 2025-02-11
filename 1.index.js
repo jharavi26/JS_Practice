86b213f999695e300607f7e2c11f1e2542cca549
Ravikumar Jha <jharavi153@gmail.com>
2025-02-11 17:05:05 +0530

    Promise

--- 1.index.js
+++ 1.index.js
@@ -1,22 +1,10 @@
-const art = document.getElementById("title");
-art.textContent = ("Welcome to the World");
-
-document.getElementById("description").style.backgroundColor = "lightgrey";
-
-let text =  document.createElement("li")
-text.innerHTML = "Item 3"
-
-const list = document.getElementById("myList")
-
-list.appendChild(text);
-
-let node = document.querySelector(".parent");
-console.log(node.parentElement)             //Accessing a parent node.
 
 
-
-
-
-
-
-
+function getData(){
+  fetch("https://dummyjson.com/users")
+  .then((res)=>res.json())
+  .then((info)=>{
+  var result = document.getElementById("output")
+  result.innerHTML = info.users.map((user,index)=>`<li>username: ${user.username}</li>`).join("")
+  })
+}
