8054727f25968677d76c08bbb5e9a9b8a8dbc172
Ravikumar Jha <jharavi153@gmail.com>
2025-02-11 17:23:34 +0530

    Add

--- 1.index.js
+++ 1.index.js
@@ -1,5 +1,4 @@
 
-
 function getData(){
   fetch("https://dummyjson.com/users")
   .then((res)=>res.json())
@@ -8,3 +7,13 @@ function getData(){
   result.innerHTML = info.users.map((user,index)=>`<li>username: ${user.username}</li>`).join("")
   })
 }
+
+async function Name (){
+  const art = await fetch("https://dummyjson.com/users");
+  const trs = await art.json();
+  console.log(trs)
+  console.log(trs.users[0].firstName)
+  var result = document.getElementById("output")
+  result.innerHTML = trs.users.map((user,index)=>`<li>username: ${user.firstName}</li>`).join("")
+}
+
